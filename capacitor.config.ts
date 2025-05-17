11d7cfa75623be8e7dc9ff0badc57e2f96340d74
GeraldFPA <GERALD.PALADINO@ucr.ac.cr>
2025-05-16 23:42:36 -0600

    se soluciona lo de los bordes de una mejor forma

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,21 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'bolsaDeEmpleoAPP',
-  webDir: 'dist',
-};
+  appId: "io.ionic.starter",
+  appName: "bolsaDeEmpleoAPP",
+  webDir: "dist",
 
+  plugins: {
+
+    StatusBar: {
+      overlaysWebView: false,
+    },
+
+    SplashScreen: {
+      launchAutoHide: true,
+    },
+    
+  },
+};
 
 export default config;
